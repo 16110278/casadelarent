ada970046f0de9aa929b958d3ee6a772674611ae
Regallia <38038568+Regallia@users.noreply.github.com>
2018-07-02 14:06:41 +0700

    tes

--- js/index.js
+++ js/index.js
@@ -121,27 +121,10 @@ $(function() {
 $(document).ready(function(){
   $(".dropdown-button").dropdown();
   $('.parallax').parallax();
-  $('select').material_select(); 
+  $('select').material_select();
+  $('.carousel').carousel(); 
   $(".button-collapse").sideNav({
   	edge: 'right'
   });  
 }) 
 
-// CAROUSEL
-$(document).ready(function(){
-  $('.carousel').carousel(
-  {
-    dist: 0,
-    padding: 0,
-    fullWidth: true,
-    indicators: true,
-    duration: 100,
-  }
-  );
-});
-
-autoplay()   
-function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 4500);
-}
