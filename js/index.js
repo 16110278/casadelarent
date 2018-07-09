02cacd4ca32d1127402445a9cebf068ad0b78a36
Regallia <38038568+Regallia@users.noreply.github.com>
2018-07-10 03:01:24 +0700

    tes

--- js/index.js
+++ js/index.js
@@ -134,9 +134,12 @@ $(document).ready(function(){
   $('select').material_select();
   $('.carousel').carousel(); 
   $('.tooltipped').tooltip();
+  $('.slider').slider();
   $(".button-collapse").sideNav({
   	edge: 'right'
   });  
 }) 
 
-
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
